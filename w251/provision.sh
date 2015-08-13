c46addfa5c68e1fbc37ed6febb68a183346559b1
david-rose <dar@acm.org>
2015-08-13 08:22:05 -0400

    provision updates

--- w251/provision.sh
+++ w251/provision.sh
@@ -4,20 +4,13 @@
 mkdir -m 777 /data
 
 # configure the second disk for use
-# locate the device name, look for the entry with the largest size
-cat /proc/partitions
- - or -
-fdisk -l | grep "2147.5 GB" | awk '{print substr($2, 0, length($2) -1)}'
- - or -
-DEV=`fdisk -l | grep "2147.5 GB" | awk '{print substr($2, 0, length($2) -1)}'`
 
-# assuming the device name is /dev/xvdc:
+DEV=`fdisk -l | grep "214.7 GB" | awk '{print substr($2, 0, length($2) -1)}'`
+
 # format the device
-mkfs.ext4 /dev/xvdc # or whichever disk is the san disk
+mkfs.ext4 $DEV
 
 # add an entry to the file system table that links the device to the data directory
-echo '/dev/xvdc /data ext4 defaults,noatime 0 0' >> /etc/fstab
- - or -
 echo "$DEV /data ext4 defaults,noatime 0 0" >> /etc/fstab
 
 # mount the directory
